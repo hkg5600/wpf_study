ce354017e0aa3044e4ea3189d297c8ea1d6d2691
hkg5600 <46782752+hkg5600@users.noreply.github.com>
2019-06-07 18:31:16 +0900

    change button design

--- BikeShop/BikeShop/Discussion.xaml.cs
+++ BikeShop/BikeShop/Discussion.xaml.cs
@@ -26,5 +26,10 @@ namespace BikeShop
             InitializeComponent();
             
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.textmsg.Text = "Hi";
+        }
     }
 }
